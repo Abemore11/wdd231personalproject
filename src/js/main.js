fcc892158de44428b13cff0677de486a70d8fb89
abemore11 <abr.moreno@outlook.com>
2026-06-20 12:35:59 -0600

    NPS6

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
-// We can do this kind of import bc we're using vite.
-// Vite let's you import CSS directly inside your JS file.
-import "../css/style.css";
+import "../css/style.css"; // we can do this because we are using Vite...
 import "../css/home.css";
-
 import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
@@ -29,30 +26,4 @@ async function init() {
   setParkInfoLinks(links);
 }
 
-function enableNavigation() {
-  const menuButton = document.querySelector("#global-nav-toggle");
-
-  // when the main menu button is clicked:
-  menuButton.addEventListener("click", (ev) => {
-    let target = ev.target;
-    // toggle the show class on the global-nav
-    document.querySelector(".global-nav").classList.toggle("show");
-    // check to see if ev.target is the button or something inside the button
-    if (target.tagName != "BUTTON") {
-      target = target.closest("button");
-    }
-    // check to see if we just opened or closed the menu
-    if (document.querySelector(".global-nav").classList.contains("show")) {
-      // if we opened it then set the aria-expanded attribute to true
-      target.setAttribute("aria-expanded", true);
-    } else {
-      // if we closed it then set the aria-expanded attribute to false
-      target.setAttribute("aria-expanded", false);
-    }
-
-    console.log("toggle");
-  });
-}
-
 init();
-enableNavigation();
